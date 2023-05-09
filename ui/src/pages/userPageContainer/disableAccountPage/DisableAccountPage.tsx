9467e39cad27f54dc228245a758d4fb43e9c54b3
namkhenh <namkhenh81@gmail.com>
2023-05-09 18:10:58 +0700

    change folder, add protected routes

---
+++ ui/src/pages/userPageContainer/disableAccountPage/DisableAccountPage.tsx
@@ -0,0 +1,106 @@
+import React, {useEffect, useState} from 'react'
+import './DisableAccountPage.scss'
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import HeaderPage from '../../structure/headerPage/HeaderPage'
+import PersonOffIcon from '@mui/icons-material/PersonOff';
+import {Dropdown} from '../../common/dropdown/DropDown';
+import {UserDisableAcount} from '../../model/apimodel/userInfo';
+import {TextField} from '../../common/textField/TextField';
+import SubmitButton from '../../common/button/SubmitButton';
+import {ButtonVariantType, LoadingPosition} from '../../model/enum/buttonEnum';
+import {useStateValue} from '../../context/StateProvider';
+import {actionType} from '../../context/Reducer';
+import {MessageBarStatus} from '../../model/enum/messageBarEnum';
+import {isStringEmpty} from '../../utils/commonFunction';
+
+function DisableAccountPage() {
+    const [loadingButton, setLoading] = useState<boolean>(false)
+    const [reason, setReason] = useState<number>(-1)
+    const [otherReason, setOtherReason] = useState<string>()
+
+    useEffect(() => {
+        setOtherReason('')
+    }, [reason])
+
+    const [, dispatch] = useStateValue()
+    const showMessageBar = (message: string, isOpen: boolean, status: MessageBarStatus) => {
+        dispatch({type: actionType.SET_MESSAGE_BAR, messageBar: {isOpen: isOpen, text: message, status: status}})
+    }
+
+    const handleUpdateInfo = () => {
+        let requestBody = {}
+        const result = new Promise((resolve) => {
+            setLoading(true)
+            setTimeout(() => {
+                setLoading(false)
+                showMessageBar("Cập nhật thông tin thành công", true, MessageBarStatus.Success)
+                resolve('success')
+            }, 4000);
+        }).then(() => {/*  */
+
+        })
+
+        return result
+    }
+    return (
+        <div className="disable-account">
+            <HeaderPage
+                icon={<ManageAccountsIcon/>}
+                text="Thiết lập tài khoản"
+                textChild="Vô hiệu hóa tài khoản"
+            />
+            <div className="disable-account-icon">
+                <PersonOffIcon/>
+            </div>
+            <div className="disable-account-note">
+                <p>namkhenh8137-0UB thân mến,</p>
+                <p>Chúng tôi rất tiếc vì tài khoản của bạn đã bị vô hiệu hóa. Sứ mệnh của chúng tôi là mang đến hành
+                    trình chăm sóc sức khỏe tiện lợi dành cho bạn. Chúng tôi luôn nỗ lực hết mình để hỗ trợ bạn, mong
+                    rằng chúng tôi sẽ có cơ hội xử lý các sự cố hoặc vấn đề khó khăn bạn đang gặp phải với tài khoản của
+                    mình.</p>
+                <p>Sau khi bị vô hiệu hóa tài khoản tại Hello Health, bạn sẽ có 30 ngày để khôi phục lại tài khoản bằng
+                    cách đăng nhập lại. Nếu bạn không thực hiện thao tác này, tài khoản của bạn với các dữ liệu, thông
+                    tin sức khỏe và lịch sử đặt khám sẽ không còn có thể truy cập hay khôi phục được nữa, đồng nghĩa với
+                    việc tài khoản sẽ bị xóa vĩnh viễn.</p>
+            </div>
+            <div className="disable-account-reason">
+                <Dropdown
+                    placeholder="- Chọn -"
+                    label="Vì sao bạn muốn vô hiệu hóa tài khoản?"
+                    options={UserDisableAcount}
+                    selectedKey={reason}
+                    required
+                    onChange={(_, selected) => {
+                        setReason(Number(selected?.key))
+                    }}
+                    // errorMessage={errorMessageString.userSex}
+                />
+            </div>
+            {reason === 2 && <TextField
+                // label='Họ và tên'
+                placeholder='Vui lòng nhập lý do của bạn'
+                required={true}
+                value={otherReason}
+                multiline
+                onChange={(_, value) => {
+                    setOtherReason(value)
+                }}
+                // errorMessage={errorMessageString.userName}
+            />}
+
+            <div className="disable-account-button">
+                <SubmitButton
+                    id={'common-dialog-default'}
+                    text={'Vô hiệu hóa tài khoản'}
+                    disable={reason === -1 || (reason === 2 && isStringEmpty(otherReason))}
+                    buttonVariantType={ButtonVariantType.Contained}
+                    promise={handleUpdateInfo}
+                    loading={loadingButton}
+                    loadingPosition={LoadingPosition.Center}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default DisableAccountPage
---
+++ ui/src/pages/userPageContainer/listAppointment/ListAppointment.tsx
@@ -0,0 +1,166 @@
+import React, {useEffect, useState} from 'react';
+import HeaderPage from '../../structure/headerPage/HeaderPage';
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
+import './ListAppointment.scss'
+import Button from '@mui/material/Button';
+import {ButtonVariantType} from '../../model/enum/buttonEnum';
+import AddIcon from '@mui/icons-material/Add';
+import {Link} from 'react-router-dom';
+import LinkOffOutlinedIcon from '@mui/icons-material/LinkOffOutlined';
+import AppointmentItem from '../../components/appointmentItem/AppointmentItem';
+import {AppointmentStatus} from '../../model/enum/appointmentEnum';
+import avatar1 from '../../base/image/anh-la-co-viet-nam-dep-1.png'
+import {IAppointmenViewModel} from '../../model/apimodel/appointmentInfo';
+import AppointmentDetail from '../../components/appointmentDetail/AppointmentDetail';
+import InfoIcon from '@mui/icons-material/Info';
+
+
+interface ListAppointmentState {
+    appointment: IAppointmenViewModel[]
+    currentSelection: number
+}
+
+const ListAppointment = (props: any) => {
+    const [state, setState] = useState<ListAppointmentState>({
+        appointment: [{
+            patientAvatar: avatar1,
+            patientName: "nguyễn văn A",
+            patientBirth: "05/18/2001",
+            patientSex: 0,
+            patientPhone: "0123456789",
+            patientAddress: {
+                province: {
+                    key: 1,
+                    text: 'Thành phố Hà Nội'
+                },
+                district: {
+                    key: 1,
+                    text: 'Quận Ba Đình'
+                },
+                commune: {
+                    key: 4,
+                    text: 'Phường Trúc Bạch'
+                }
+            },
+            appointmentDate: "06/12/2003",
+            appointmentTime: 0,
+            appointmentReason: "đau đầu ù tai",
+            appointmentStatus: AppointmentStatus.Success,
+            appointmentId: "DK123456",
+            appointmentNumber: 3
+        },
+            {
+                patientAvatar: avatar1,
+                patientName: "nguyễn văn X",
+                patientBirth: "05/18/2001",
+                patientSex: 1,
+                patientPhone: "0123456789",
+                patientAddress: {
+                    province: {
+                        key: 1,
+                        text: 'Thành phố Hà Nội'
+                    },
+                    district: {
+                        key: 1,
+                        text: 'Quận Ba Đình'
+                    },
+                    commune: {
+                        key: 4,
+                        text: 'Phường Trúc Bạch'
+                    }
+                },
+                appointmentDate: "06/12/2003",
+                appointmentTime: 2,
+                appointmentReason: "đau đầu ù tai",
+                appointmentStatus: AppointmentStatus.Waiting,
+                appointmentId: "DK123456",
+                appointmentNumber: 3
+            }],
+        currentSelection: -1
+    })
+
+    const {appointment, currentSelection} = state
+    useEffect(() => {
+
+    }, [])
+
+    const handleClickAppointment = (index: number) => {
+        setState({
+            ...state,
+            currentSelection: index
+        });
+    }
+
+    return (
+        <div id='user-appointment' className='user-appointment'>
+            <HeaderPage icon={<EventAvailableIcon/>} text='Quản lý lịch khám' textChild='Danh sách lịch khám'/>
+            <div className="appointment-container">
+                <div className="list-appointment">
+                    <div className="appointment-title">
+                        Danh sách lịch khám
+                    </div>
+                    <div className="appointment-sub-title">
+                        Vui lòng chọn một trong các lịch khám có sẵn để xem chi tiết hoặc bấm vào <strong>Đặt khám
+                        mới</strong> để tạo lịch khám mới
+                    </div>
+                    <div className="list-appointment-wrap">
+                        {/* <div className="no-appointment">
+                            <ReportProblemOutlinedIcon sx={{ fontSize: '96px', color: '#cccccc', marginBottom: '12px' }}></ReportProblemOutlinedIcon>
+                            Không tìm thấy lịch hẹn nào
+                        </div> */}
+                        {appointment?.map((item, index) => {
+                            return <AppointmentItem
+                                index={index}
+                                avatar={item.patientAvatar}
+                                name={item.patientName}
+                                time={item.appointmentTime}
+                                date={item.appointmentDate}
+                                status={item.appointmentStatus!}
+                                selectAppointment={handleClickAppointment}
+                                isSelected={currentSelection === index}
+                            />
+                        })}
+                    </div>
+                    <div className="booking-new-appointment">
+                        <Button variant={ButtonVariantType.Outlined} startIcon={<AddIcon/>}>
+                            <Link to='/quan-ly/quan-ly-lich-kham/dat-lich-kham' style={{color: '#1976d2'}}>
+                                Đặt khám mới
+                            </Link>
+                        </Button>
+                    </div>
+                </div>
+                <div className="list-appointment-right">
+                    <div className="list-appointment-detail">
+                        <div className="appointment-title">
+                            Chi tiết lịch khám
+                        </div>
+                        <div className="list-appointment-wrap">
+                            {currentSelection === -1 ?
+                                <div className="no-list-appointment">
+                                    <LinkOffOutlinedIcon sx={{
+                                        fontSize: '96px',
+                                        color: '#cccccc',
+                                        marginBottom: '12px'
+                                    }}></LinkOffOutlinedIcon>
+                                    Vui lòng chọn một lịch khám để xem chi tiết
+                                </div> :
+                                <AppointmentDetail appointment={appointment[currentSelection]}/>
+                            }
+                        </div>
+                    </div>
+                    {currentSelection !== -1 &&
+                        <div className="appointment-hotline">
+                            <InfoIcon sx={{fontSize: '14px', color: '#FFA95A'}}/>
+                            <div className="hotline">
+                                Lịch khám của bạn đã được gửi đi. Vui lòng đợi <strong>Riordan Clinic</strong> xác nhận.
+                                Mọi yêu cầu về <strong>Hủy lịch/ Đổi lịch</strong> vui lòng gọi
+                                Hotline <strong>19001234</strong> để được hỗ trợ.<br/>Trân trọng cảm ơn!
+                            </div>
+                        </div>}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ListAppointment;
